80a5f82a1de90f04b76bea786aa5ba570979faad
elysiaq <97921607+elysiaq@users.noreply.github.com>
2024-01-23 18:42:57 -0600

    basic todo

---
+++ todo.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>ToDo App</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <?php
+        require 'database.php';
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $new = (string)$_POST['new'];
+
+            $stmt = $mysqli->prepare("insert into todos (title) values (?)");
+            if (!$stmt) {
+                printf("Query Prep Failed: %s\n", $mysqli->error);
+                exit;
+            }
+
+            $stmt->bind_param('s', $new);
+            
+            if ($stmt->execute()) {
+                $creation_successful = true;
+                header("Location: {$_SERVER['REQUEST_URI']}");
+                exit();
+            }
+
+            $stmt->close();
+        }
+        ?>
+
+        <h1>ToDo List:</h1>
+        <form method="POST" action="todo.php">
+            <label for="new">Add new todo:</label>
+            <input type="text" id="new" name="new" required>
+        </form>
+
+        <?php
+            $stmt = $mysqli->prepare("select title from todos");
+            $stmt->execute();
+            $stmt->bind_result($item);
+
+            while($stmt->fetch()){
+                echo '<input type="checkbox">';
+                echo '<span>'.htmlentities($item).'</span><br>';
+            }
+            $stmt->close();
+        ?>
+
+    </body>
+</html>
